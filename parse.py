e3a6b78c5d534aad1da3cf186e79baecf5263c3e
Furkan679 <furkan.t0704@gmail.com>
2020-06-08 11:48:38 +0200

    hallo

---
+++ parse.py
@@ -0,0 +1,24 @@
+
+def parse_xml(label_path):
+    x = xmltodict.parse(open(label_path , 'rb'))
+    item_list = x['annotation']['object']
+    
+    # when image has only one bounding box
+    if not isinstance(item_list, list):
+        item_list = [item_list]
+        
+    result = []
+    
+    for item in item_list:
+        name = item['name']
+        bndbox = [(int(item['bndbox']['xmin']), int(item['bndbox']['ymin'])),
+                  (int(item['bndbox']['xmax']), int(item['bndbox']['ymax']))]       
+        result.append((name, bndbox))
+    
+    size = [int(x['annotation']['size']['width']), 
+            int(x['annotation']['size']['height'])]
+    
+    return result
+
+x = parse_xml("C:/Users/Furkan1/Desktop/bwki/datasets/1/labels/_2019101018405898714.xml")
+
