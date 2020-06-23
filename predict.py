d64e1558e37231a24f86681767917949c4022e9a
Furkan679 <furkan.t0704@gmail.com>
2020-06-23 22:08:04 +0200

    Update predict.py

--- predict.py
+++ predict.py
@@ -48,23 +48,23 @@ while(True):
 	face_location = eye_cascade.detectMultiScale(image,1.3,5) # mit Hilfe des Klassifizierers werden alle Gesichter erkannt und wie folgt gespeichert:
 	# [(Gesicht 1), (Gesicht 2), ..., (Gesicht n)]
 	#  (x0 Koordinate des oberen linken Punktes des Rechtecks, das das Gesicht umrahmt; y0; Länge in x Richtung von x0 aus; Länge in y Richtung von y0 aus)
-
+	
 	#for (x,y,w,h) in face_location: # Für jedes Tupel aller gesichteten Gesichter wird folgendes gemacht:
 	if len(face_location) == 2:
 		(x, y, w, h) = getleftmosteye(face_location)
 		new_image = image[y-20:y+w+100, x-20:x+w+100] # Das Bild wird zugeschnitten, damit man das Gesicht allein untersuchen kann
-
+		
 		pred = predict(new_image) # Das zugeschnittene Bild wird nun getestet
 		print(pred)
 		text = 'Mask' if pred == 1 else 'No Mask' # Der Text, der ausgegeben wird ist 'Mask', wenn predict(new_image) 1 zurückgibt, 0 andernfalls
-
+		
 		cv2.rectangle(image, (x-20, y-20), (x+w+100, y+h+100), (0, 255, 0)) # jetzt wird ein Rechteck aufs Bild image gemalt in der Form:
 		# cv2.rectangle(Bild, auf das das RE gemalt wird; (Koordinaten des linken oberen Punktes), (Verschiebung in x und y Richtung), (Farbe als RGB-Wert))
-
+		
 		cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0))
 		# nun wird der text über das RE geschrieben
-
+		
 	cv2.imshow('frame', image) # Diese Methode erzeugt ein Fenster und Inhalt ist das aktuelle Bild, der Name des Fensters ist 'frame'
-
+	
 	if cv2.waitKey(1) & 0xFF == ord('q'): # 'Wenn 'q' gedrückt wird, schließe alles'
 		break
