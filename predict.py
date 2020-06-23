9838c9215c49dda7033f61d0ee902f98881dab24
Furkan679 <furkan.t0704@gmail.com>
2020-06-23 23:18:50 +0200

    can detect more than one face now

--- predict.py
+++ predict.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 import cv2 
 import face_recognition 
 from PIL import Image 
- 
+import math
 
 #zwingt das System, die Grafikkarte zu benutzen
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
@@ -19,13 +19,65 @@ eye_cascade = cv2.CascadeClassifier('classifier/lefteye.xml')
 
 model = tf.keras.models.load_model('model') #lädt hiermit das zuvor trainierte Model
 
+def getShortestEye(eye, eyes):
+	shortest = 1000000
+	#for i in range(len(eyes)):
+	#midCurrent = ((eye[0] + eye[2])/2, (eye[1] + eye[3])/2)
+	midCurrent = (eye[0], eye[1])
+
+	midOthers = []
+
+	#midOthers.append(((2*eyes[0][0] + eyes[0][2])/2, (2*eyes[0][2] + eyes[0][3])/2)) 
+	#midOthers.append(((2*eyes[1][0] + eyes[1][2])/2, (2*eyes[1][2] + eyes[1][3])/2)) 
+	#midOthers.append(((2*eyes[2][0] + eyes[2][2])/2, (2*eyes[2][2] + eyes[2][3])/2)) 
+	#midOthers.append(((2*eyes[3][0] + eyes[3][2])/2, (2*eyes[3][2] + eyes[3][3])/2)) 
+
+	#midOthers.append((eyes[0][0], (eyes[0][1]))) 
+	eyes = eyes.tolist()
+
+	for eye in eyes:
+		#print(eye[0])
+		midOthers.append((eye[0], eye[1])) 
+
+	distances = []
+
+	for distance in midOthers:
+		currentDistance = math.sqrt((distance[0] - midCurrent[0])**2 + (distance[1] - midCurrent[1])**2)
+		distances.append(currentDistance)
+		#print(currentDistance)
+		if currentDistance < shortest and currentDistance != 0:
+			shortest = currentDistance
+
+	return midOthers[int(distances.index(shortest))], int(distances.index(shortest))
+
+#print(groupEyePairs((0, 0, 2, 2), [(1, 1, 2, 2), (2, 2, 2, 2), (3, 3, 2, 2), (4, 4, 2, 2)]))
+
+def groupEyes(eyes):
+	#print(type(eyes))
+	new_eyes = []
+	for eye in eyes:
+			if len(new_eyes) == 0:
+				new_eyes.append((eye, (eyes[getShortestEye(eye, eyes)[1]])))
+
+			else:
+				for left, right in new_eyes:
+					#print(left, right)
+					#print(eye)
+					if eye.tolist() == left.tolist() or eye.tolist() == right.tolist():
+						#print(1)
+						break
+					break
+				new_eyes.append((eye, (eyes[getShortestEye(eye, eyes)[1]])))
+			
+	return new_eyes[::2]
+
 def getleftmosteye(eyes):
-	leftmost=9999999
-	leftmostindex=-1
+	leftmost = 9999999
+	leftmostindex = -1
 	for i in range(0,2):
-		if eyes[i][0]<leftmost:
-			leftmost=eyes[i][0]
-			leftmostindex=i
+		if eyes[i][0] < leftmost:
+			leftmost = eyes[i][0]
+			leftmostindex = i
 	return eyes[leftmostindex]
 
 #definiert die Methode 'predict', die 1 für Maske und 0 für keine Maske zurückgibt
@@ -48,23 +100,30 @@ while(True):
 	face_location = eye_cascade.detectMultiScale(image,1.3,5) # mit Hilfe des Klassifizierers werden alle Gesichter erkannt und wie folgt gespeichert:
 	# [(Gesicht 1), (Gesicht 2), ..., (Gesicht n)]
 	#  (x0 Koordinate des oberen linken Punktes des Rechtecks, das das Gesicht umrahmt; y0; Länge in x Richtung von x0 aus; Länge in y Richtung von y0 aus)
-	
+
 	#for (x,y,w,h) in face_location: # Für jedes Tupel aller gesichteten Gesichter wird folgendes gemacht:
-	if len(face_location) == 2:
-		(x, y, w, h) = getleftmosteye(face_location)
-		new_image = image[y-20:y+w+100, x-20:x+w+100] # Das Bild wird zugeschnitten, damit man das Gesicht allein untersuchen kann
-		
-		pred = predict(new_image) # Das zugeschnittene Bild wird nun getestet
-		print(pred)
-		text = 'Mask' if pred == 1 else 'No Mask' # Der Text, der ausgegeben wird ist 'Mask', wenn predict(new_image) 1 zurückgibt, 0 andernfalls
-		
-		cv2.rectangle(image, (x-20, y-20), (x+w+100, y+h+100), (0, 255, 0)) # jetzt wird ein Rechteck aufs Bild image gemalt in der Form:
-		# cv2.rectangle(Bild, auf das das RE gemalt wird; (Koordinaten des linken oberen Punktes), (Verschiebung in x und y Richtung), (Farbe als RGB-Wert))
-		
-		cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0))
-		# nun wird der text über das RE geschrieben
-		
+	if len(face_location) >= 2:
+		pairs = groupEyes(face_location)
+		new_eyes = []
+		for i in pairs:
+			new_eyes.append(getleftmosteye(i))
+
+		#print(new_eyes)
+		for eye in new_eyes:
+			(x, y, w, h) = eye
+			new_image = image[y-20:y+w+100, x-20:x+w+100] # Das Bild wird zugeschnitten, damit man das Gesicht allein untersuchen kann
+
+			pred = predict(new_image) # Das zugeschnittene Bild wird nun getestet
+			#print(pred)
+			text = 'Mask' if pred == 1 else 'No Mask' # Der Text, der ausgegeben wird ist 'Mask', wenn predict(new_image) 1 zurückgibt, 0 andernfalls
+
+			cv2.rectangle(image, (x-20, y-20), (x+w+100, y+h+100), (0, 255, 0)) # jetzt wird ein Rechteck aufs Bild image gemalt in der Form:
+			# cv2.rectangle(Bild, auf das das RE gemalt wird; (Koordinaten des linken oberen Punktes), (Verschiebung in x und y Richtung), (Farbe als RGB-Wert))
+
+			cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0))
+			# nun wird der text über das RE geschrieben
+
 	cv2.imshow('frame', image) # Diese Methode erzeugt ein Fenster und Inhalt ist das aktuelle Bild, der Name des Fensters ist 'frame'
-	
+
 	if cv2.waitKey(1) & 0xFF == ord('q'): # 'Wenn 'q' gedrückt wird, schließe alles'
 		break
